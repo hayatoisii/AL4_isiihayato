9d147f9416f894690a09e3d8b27bad6d3283ec98
hayatoisii <k023g0022@g.neec.ac.jp>
2024-10-10 13:03:14 +0900

    mikannsei

--- DirectXGame/Enemy.cpp
+++ DirectXGame/Enemy.cpp
@@ -1,32 +1,63 @@
 #include "Enemy.h"
 #include <cassert>
 
-Enemy::~Enemy() {}
+Enemy::~Enemy() {
+
+	delete modelbullet_;
+	for (EnemyBullet* bullet : bullets_) {
+		delete bullet;
+	}
+}
 
 void Enemy::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const KamataEngine::Vector3& pos) {
 	assert(model);
 	model_ = model;
 	camera_ = camera;
+	modelbullet_ = KamataEngine::Model::CreateFromOBJ("cube", true);
 	worldtransfrom_.translation_ = pos;
 	worldtransfrom_.Initialize();
+}
+
+void Enemy::Fire() {
+
+	KamataEngine::Vector3 moveBullet = {0, 0, 0};
+
+	moveBullet = worldtransfrom_.translation_;
+
+	// 弾の速度
+	const float kBulletSpeed = 1.0f;
+	KamataEngine::Vector3 velocity(0, 0, kBulletSpeed);
+
+	// 弾を生成し、初期化
+	EnemyBullet* newBullet = new EnemyBullet();
+	newBullet->Initialize(modelbullet_, moveBullet, velocity);
+
+	// 弾を登録する
+	bullets_.push_back(newBullet);
+}
+
+void Enemy::Bulletaccess() {
+	// 発射タイマーを初期化
+	spawnTimer = kFireInterval;
+	
+	spawnTimer--;
+
+	if (spawnTimer < 0.0f) {
+		// 弾を発射
+		Fire();
+		spawnTimer = kFireInterval;
+	}
 }
 
 void Enemy::Update() {
 
 	// キャラクターの移動ベクトル
 	KamataEngine::Vector3 move = {0, 0, 0};
-
-	// キャラクターの移動速さ
-	//const float kCharacterSpeed = 0.3f;
 	
 	// 接近
 	KamataEngine::Vector3 accessSpeed = {0.3f,0.3f,0.3f};
 	// 離脱
 	KamataEngine::Vector3 eliminationSpeed = {0.3f, 0.3f, 0.3f};
-
-	//worldtransfrom_.translation_.x += move.x;
-	//worldtransfrom_.translation_.y += move.y;
-	//worldtransfrom_.translation_.z += move.z;
 
 	switch (phase_) { 
 	case Phase::Approach:
@@ -40,7 +71,7 @@ void Enemy::Update() {
 		break;
 	case Phase::Leave:
 		// 移動(ベクトルを加算)
-		worldtransfrom_.translation_.y += eliminationSpeed.y;
+		//worldtransfrom_.translation_.y += eliminationSpeed.y;
 		break;
 
 	}
--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -2,6 +2,7 @@
 #include <3d/Model.h>
 #include <3d/WorldTransform.h>
 #include <3d/Camera.h>
+#include "EnemyBullet.h"
 
 enum class Phase {
 	Approach, // 接近する
@@ -15,6 +16,11 @@ public:
 	void Update();
 	void Draw();
 	~Enemy();
+	void Fire();
+	void Bulletaccess();
+
+	// 発射間隔
+	static const int kFireInterval = 60;
 
 private:
 
@@ -22,6 +28,16 @@ private:
 	KamataEngine::Model* model_ = nullptr;
 	KamataEngine::Camera* camera_ = nullptr;
 
+	KamataEngine::Model* modelbullet_ = nullptr;
+
+	// 弾
+	std::list<EnemyBullet*> bullets_;
+
+	// 発射タイマー
+	int32_t spawnTimer = 0;
+
 	Phase phase_ = Phase::Approach;
+
+	Phase Bulletphase_ = Phase::Approach;
 
 };
